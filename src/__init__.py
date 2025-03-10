a2faf2d436ced2d4828d5ed34027c468d5b670fa
Adam Jurčo <adamjurco@gmail.com>
2025-03-10 15:41:17 +0100

    Added permissions.

--- src/__init__.py
+++ src/__init__.py
@@ -16,10 +16,10 @@ atexit.register(Rajce.cleanup)
 # set popups
 Rajce.homepage_popups = [RajceButton.COOKIES_AGREEMENT]
 Rajce.download_folder = Path.home() / Path('Downloads')
-Rajce.download_folder.chmod(0o755)
+os.chmod(Rajce.download_folder, 0o755)
 
 # set chromedriver
 Rajce.driver_file = Path(__file__).parent.parent / Path(r'drivers/chromedriver.exe')
-os.chmod(str(Rajce.driver_file), 0o755)
+os.chmod(Rajce.driver_file, 0o755)
 # driver options
 Rajce.driver_options = CONFIG.get('driver_options')
