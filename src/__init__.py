7f4855495f6dbb26d7613e60b339e386e049dd46
Adam Jurčo <adamjurco@gmail.com>
2025-03-12 18:11:36 +0100

    Added chromedriver to setup.

--- src/__init__.py
+++ src/__init__.py
@@ -9,20 +9,19 @@ from pathlib import Path
 
 
 CONFIG = InOut.read_json(file=Path(__file__).parent.parent / Path(r'config/config.json'))
-
 Logger.config = CONFIG['logger']
 # rajce
 atexit.register(Rajce.cleanup)
+
 # set popups
 Rajce.homepage_popups = [RajceButton.COOKIES_AGREEMENT]
-# task executor
-TaskExecutor.task_file = Path(__file__).parent.parent / Path(r'config/rajce_taskfile.txt')
-# set chromedriver
-if os.name == 'nt':  # Windows
-  Rajce.driver_file = Path(__file__).parent.parent / Path(r'drivers/chromedriver.exe')
-else:
-  Rajce.driver_file = Path(__file__).parent.parent / Path(r'drivers/chromedriver')
 
+# set chromedriver
+Rajce.driver_file = Path(CONFIG['driver_path']) # Path(__file__).parent.parent / Path(r'drivers/chromedriver')
 os.chmod(Rajce.driver_file, 0o755)
+
 # driver options
 Rajce.driver_options = CONFIG.get('driver_options')
+
+# task executor
+TaskExecutor.task_file = Path(__file__).parent.parent / Path(r'config/rajce_taskfile.txt')
