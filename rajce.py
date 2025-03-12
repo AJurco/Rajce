d470862b3e9d7c0165dc51ad21635f088aef7397
Adam Jurčo <adamjurco@gmail.com>
2025-03-12 18:21:47 +0100

    Added rajce.py

---
+++ rajce.py
@@ -0,0 +1,19 @@
+from src import Rajce, Logger, TaskExecutor
+
+logger = Logger(name=__name__, level='INFO')
+
+def main():
+  Rajce.start_driver()
+  Rajce.set_user('salomaaje91')
+  logger.info(F'User set to {Rajce.user}.')
+  # get album -> date mapping
+  mapping = Rajce.save_albumdate_mapping()
+  tasks = Rajce.get_image_tasks(album_date_mapping=mapping)
+  TaskExecutor.init(tasks)
+  TaskExecutor.run_tasks(task_performer=Rajce.task_performer)
+
+
+if __name__ == '__main__':
+  main()
+  Rajce.cleanup()
+  logger.info('Rajce cleanup complete.')
--- src/main.py
+++ src/main.py
@@ -247,20 +247,3 @@ class Rajce:
     cls.user = user
     cls.homepage = f"https://{cls.user}.rajce.idnes.cz/"
     cls.output_folder = cls.download_folder / Path(f'rajce_{user}')
-
-
-def main():
-  Rajce.start_driver()
-  Rajce.set_user('salomaaje91')
-  logger.info(F'User set to {Rajce.user}.')
-  # get album -> date mapping
-  mapping = Rajce.save_albumdate_mapping()
-  tasks = Rajce.get_image_tasks(album_date_mapping=mapping)
-  TaskExecutor.init(tasks)
-  TaskExecutor.run_tasks(task_performer=Rajce.task_performer)
-
-
-if __name__ == '__main__':
-  main()
-  Rajce.cleanup()
-  logger.info('Rajce cleanup complete.')
